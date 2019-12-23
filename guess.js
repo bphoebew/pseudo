b1bed35a0aac670699b2f46fabc68f01fc77cc52
bphoebew <bphoebew@gmail.com>
2019-12-23 00:06:55 -0800

    commit 22

--- guess.js
+++ guess.js
@@ -4,10 +4,10 @@ var container = $('.container');
 submit.on("click", showResult);
 
 function showResult(){
-  if (document.querySelector(".alexei").checked ==true && document.querySelector(".jocelyn").checked == true){
+  if (document.querySelector(".alexei").checked ==true && document.querySelector(".jocelyn").checked == true && document.querySelector(".lily").checked == true){
     container.empty();
     container.append(`<p class = "answer"> Congrats you've found all the catfishers! </p>`);
-}else if (document.querySelector(".alexei").checked ==true || document.querySelector(".jocelyn").checked == true){
+}else if (document.querySelector(".alexei").checked ==true || document.querySelector(".jocelyn").checked == true || document.querySelector(".lily").checked == true){
     container.empty();
     container.append(`<p class = "answer">You've caught one catfisher!! There's one more!</p>`);
 }else if(document.querySelector(".adrien").checked ==true || document.querySelector(".lily").checked == true || document.querySelector(".nax").checked == true || document.querySelector(".kai").checked == true){
